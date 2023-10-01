770795d15266472f06bdf879aa1bac609904fa3f
Yance24 <yachzeet@gmail.com>
2023-10-01 13:43:22 +0700

    Update yazee's mini project
    just to backup

---
+++ testPHP/yazee's stuff/testyazee.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+
+    <?php
+    //output
+    echo "== output ==<br>";
+    echo "uses echo / echo() and <br>print() to display text on a website<br>";
+    echo("tes echo()<br>");
+    print("tes print()<br>");
+    ?>
+
+    <br>
+
+    <?php
+    //variable
+    echo "== variable ==<br>";
+    $x = 10;
+    $y = 20;
+    $result = $x + $y;
+    echo "x = ".$x."<br>";
+    echo "y = ".$y."<br>";
+    echo "result = ".$result;
+    ?>
+
+</body>
+
+</html>
