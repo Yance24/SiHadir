8a4a550f92180f3dea6fb50c63a374c07315b881
ashaf12345 <ashafmaulana12@gmail.com>
2023-10-13 10:57:34 +0700

    mvc
    membuat program sederhana dengan mvc

---
+++ testPHP/ashaf/mvc/model.php
@@ -0,0 +1,15 @@
+<?php
+class Model
+{
+    private $tasks = [];
+
+    public function addTask($task)
+    {
+        $this->tasks[] = $task;
+    }
+
+    public function getTasks()
+    {
+        return $this->tasks;
+    }
+}
