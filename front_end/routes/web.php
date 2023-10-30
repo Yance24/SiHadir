8be4798c5e009e75be30d6d0f4a97a28cc4b8e37
Rudiyat <119860950+pddccvv@users.noreply.github.com>
2023-10-30 16:15:44 +0700

    Update web.php
    route pemindai

--- front_end/routes/web.php
+++ front_end/routes/web.php
@@ -22,19 +22,34 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
     });
     Route::get('/dosen/dashboard',[DashboardController::class,'processDosenView']);
 
-    //Route untuk mahasiswa
-    Route::get('/mahasiswa/profil', function(){
-        return view ('mahasiswa.profil');
-    });
-    Route::get('/mahasiswa/perizinan', function(){
-        return view ('mahasiswa.perizinan');
-    });
+    // MAHASISWA
+
     Route::get('/mahasiswa/profil', function(){
         return view('mahasiswa.profil');
     });
+    Route::get('/mahasiswa/perizinan', function(){
+        return view('mahasiswa.perizinan');
+    });
+
     Route::get('/mahasiswa/dashboard', function(){
         return view('mahasiswa.dashboard');
     });
+
+    Route::get('/mahasiswa/profil', function(){
+        return view('mahasiswa.profil');
+    });
+
+    Route::post('scanner', [PemindaiController::class, 'scanner']);
+
+    Route::get('/mahasiswa/test', function(){
+        return view('mahasiswa.test');
+    });
+
+    Route::get('/pemindai', 'PemindaiController@scanner')->name('pemindai::class.scanner');
+
+    Route::view('/profil', 'NavigasiController@profil')->name('profil::class.profil');
+
+    // END
 
     //Route untuk admin
     Route::get('/admin/jadwal-akademik','App\Http\Controllers\SiHadirController@index');
