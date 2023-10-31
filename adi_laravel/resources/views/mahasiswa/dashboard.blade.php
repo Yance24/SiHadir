1ab818cb80b2ca3d9ca587021e17f1a7a2e20499
adisuryadi06 <adisuryadi060503@gmail.com>
2023-10-31 15:01:36 +0700

    update data

--- adi_laravel/resources/views/mahasiswa/dashboard.blade.php
+++ adi_laravel/resources/views/mahasiswa/dashboard.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.appbarMobile')
 
+@section('layouts.title', 'Dashboard')
+
 @section('extra-css-js')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -9,13 +11,41 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous">
+
+    <script>
+        // Fungsi untuk menampilkan popup SweetAlert2
+        document.getElementById('logoutLink').addEventListener('click', function(event) {
+            event.preventDefault(); // Mencegah perilaku default dari tag anchor
+
+            Swal.fire({
+                title: 'Ingin keluar dari aplikasi?',
+                icon: 'warning',
+                showCancelButton: true, // Menampilkan tombol "Batal"
+                confirmButtonColor: '#7ACC78',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'YA',
+                cancelButtonText: 'TIDAK',
+                reverseButtons: true // Memutar urutan tombol
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Menangani klik tombol "Ya"
+                    Swal.fire('Anda telah keluar', '', 'success');
+                    // Tambahkan fungsi logout atau redirect ke halaman logout di sini
+                    window.location.href = "../login"; // Ganti dengan URL logout Anda
+                } else {
+                    // Menangani klik tombol "Batal"
+                    // Lakukan sesuatu atau berikan perilaku kustom
+                }
+            });
+        });
+    </script>
     </section>
 
-    <div class="content" style="margin-top:100px;">
-        <div class="mb-5" style="padding-left: 25px;">
+    <div class="content container" style="margin-top:100px; overflow-x:hidden;">
+        <div class="mb-5 pl-3 pt-3" style="">
             <span style="font-size: 14px;">halo, <b>Weldy Flaminggo</b></span>
         </div>
-        <div class="main-content" style="margin-left: 25px">
+        <div class="main-content">
             <div class="row">
                 <div class="col-md-12">
                     <div class="row">
@@ -31,48 +61,47 @@
                                 <h1 class="ml-lg-2 fw-bold" style="font-size: 20px;">PBL</h1>
                                 <h1 class="ml-lg-2" style="font-size: 20px;">07.00 - 12.00</h1>
                                 <hr>
-                                <div class="text-center" id="hideCurrent">
-                                    <button id="absenButton" class="btn btn-primary shadow-lg rounded-3"
-                                        style="width: 200px; height: 50px">
-                                        Absen
-                                    </button>
-                                </div>
+                                <form action="/scanner" method="POST">
+                                    @csrf
+                                    <div class="text-center">
+                                        <button id="absenButton" class="btn btn-primary shadow-lg rounded-3"
+                                            style="width: 200px; height: 50px">
+                                            Absen
+                                        </button>
+                                    </div>
+                                </form>
                             </div>
-                            <!-- END -->
-
-                            <!-- NEXT JADWAL -->
-                            <div class="mb-3">
-                                <img src="{{ asset('assets/icon/table.svg') }}" alt="">
-                                <span class="fw-bold" style="font-size: 18px;">Jadwal Selanjutnya</span>
-                            </div>
-                            <div class="col-sm-11 p-5 flex justify-content-center justify-content-lg-start mb-5"
-                                style="
-                                box-shadow: 0px 4px 4px 3px rgba(0, 0, 0, 0.25); border-radius: 15px; border: 1px solid #78A2CC; ">
-                                <h1 class="ml-lg-2 fw-bold" style="font-size: 20px;">PEMOGRAMAN WEB</h1>
-                                <h1 class="ml-lg-2" style="font-size: 20px;">07.00 - 12.00</h1>
-                                <hr>
-                                <div class="text-center" id="hideNext">
-                                    <button class="btn btn-primary shadow-lg rounded-3" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseCurrent1" id="scanQrButton"
-                                        style="width: 200px; height: 50px">
-                                        Absen
-                                    </button>
-                                </div>
-                            </div>
-                            <!-- END -->
                         </div>
+                        <!-- END -->
+
+                        <!-- NEXT JADWAL -->
+                        <div class="mb-3">
+                            <img src="{{ asset('assets/icon/table.svg') }}" alt="">
+                            <span class="fw-bold" style="font-size: 18px;">Jadwal Selanjutnya</span>
+                        </div>
+                        <div class="col-sm-11 p-5 flex justify-content-center justify-content-lg-start mb-5"
+                            style="
+                                box-shadow: 0px 4px 4px 3px rgba(0, 0, 0, 0.25); border-radius: 15px; border: 1px solid #78A2CC; ">
+                            <h1 class="ml-lg-2 fw-bold" style="font-size: 20px;">PEMOGRAMAN WEB</h1>
+                            <h1 class="ml-lg-2" style="font-size: 20px;">07.00 - 12.00</h1>
+                            <hr>
+                            <div class="text-center" id="hideNext">
+                                <form action="/scanner" method="POST">
+                                    @csrf
+                                    <div class="text-center">
+                                        <button id="absenButton" class="btn btn-primary shadow-lg rounded-3"
+                                            style="width: 200px; height: 50px">
+                                            Absen
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- END -->
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    {{-- <script>
-        // UNTUK PINDAH KE PEMINDAI PAGE - UNTUK TOMBOL ABSEN
-       /* document.addEventListener('DOMContentLoaded', function() {
-            const absenButton = document.getElementById('absenButton');
-    
-            absenButton.addEventListener('click', function() {
-                window.location.href = "{{ route('mahasiswa.barcode.index') }}";
-            });
-        }); */
-    </script> --}}
+    </div>
+    <script></script>
