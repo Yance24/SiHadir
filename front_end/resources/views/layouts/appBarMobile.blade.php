07d8ce2956712e594c7d13311789ffc695d073cf
Rudiyat <119860950+pddccvv@users.noreply.github.com>
2023-11-06 11:37:02 +0700

    revisi

--- front_end/resources/views/layouts/appBarMobile.blade.php
+++ front_end/resources/views/layouts/appBarMobile.blade.php
@@ -11,188 +11,62 @@
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer">
 
-    <style>
-        .appbar {
-            display: flex;
-            height: 100px;
-            background-color: #78A2CC;
-            position: fixed;
-            width: 100%;
-            z-index: 1000;
-        }
+    <link rel="stylesheet" href="{{ asset('assets/css/layouts/appbarMobile.css') }}">
 
-        .sidebar {
-            height: 100%;
-            width: 0;
-            position: fixed;
-            top: 100px;
-            left: 0;
-            background-color: #78A2CC;
-            overflow-x: hidden;
-            transition: 0.5s;
-            z-index: 1;
-        }
-
-        .sidebar a {
-            padding: 15px;
-            text-decoration: none;
-            font-size: 25px;
-            color: #ffffff;
-            display: block;
-            transition: 0.3s;
-        }
-
-        .custom-profile {
-            background: url({{ asset('assets/img/bg-profile.png') }});
-            background-size: cover;
-            background-position: center;
-        }
-
-        .sidebar a:hover {
-            color: #78A2CC;
-        }
-
-        #menuToggle {
-            display: none;
-        }
-
-        #menuToggle:checked+.appbar+.content-fluid {
-            margin-left: 200px;
-        }
-
-        #menuToggle:checked+.appbar+.sidebar {
-            width: 55%;
-        }
-
-        #menuToggle:checked+.appbar label {
-            background-color: transparent;
-        }
-
-        #menuToggle:checked+.appbar label i {
-            color: #ffffff;
-        }
-
-        #menuToggle:checked+.appbar label:hover i {
-            color: #78A2CC;
-        }
-
-        .bg-custom {
-            background: url({{ asset('assets/img/bg-profile.png') }}) center no-repeat;
-            background-size: cover;
-        }
-
-        @media (max-width: 1024px) {
-
-            #menuToggle:checked+.appbar+.sidebar {
-                width: 55%;
-            }
-
-            .nav-custom {
-                font-size: 50px;
-            }
-        }
-
-        @media (max-width: 767px) {
-
-            #menuToggle:checked+.appbar+.sidebar {
-                width: 80%;
-            }
-
-            .nav-custom {
-                font-size: 10px;
-            }
-        }
-    </style>
 </head>
 
 <body>
-    <input type="checkbox" id="menuToggle">
-    <div class="appbar fixed-top">
-        <label for="menuToggle" class="mt-5 ps-2">
-            <i class="fa-solid fa-2xl fa-bars" style="color: #ffffff;"></i>
-        </label>
-        <div class="mx-auto mt-5">
-            <h1 class="text-white">Sihadir</h1>
-        </div>
-    </div>
-
-    <div class="sidebar text-white">
-        <div class="header-box custom-profile mb-3 pb-3">
-            <div class="mb-2 p-3" style="z-index: 2">
-                <img src="{{ asset('assets/img/profile-pict.png') }}" alt="profile" class="rounded-circle"
-                    width="80px" height="80px">
-            </div>
-            <div class="p-2" style="z-index: 2">
-                <span class="fw-bold nama">Weldy Flaminggo</span><br>
-                <span class="nim">123456789</span>
+    <header>
+        <input type="checkbox" id="menuToggle">
+        <div class="appbar fixed-top">
+            <label for="menuToggle" class="mt-4 ps-2 pe-auto">
+                <i class="fa-solid fa-2xl fa-bars pt-3" style="color: #ffffff;"></i>
+            </label>
+            <div class="mx-auto mt-4">
+                <h1 class="text-white">SiHadir</h1>
             </div>
         </div>
 
-        <div class="d-flex flex-column">
-            <div>
-                <a href="{{ route('dashboard') }}" class="text-decoration-none text-white">
-                    <img src="{{ asset('assets/icon/absensi.png') }}" alt="" class="nav-custom">
-                    <span style="font-size: 15px"> Absen</span>
-                </a>
-            </div>
-            <div>
-                <a href="{{ route('profil') }}" class="text-decoration-none text-white ">
-                    <img src="{{ asset('assets/icon/profil.png') }}" alt="" class="nav-custom">
-                    <span style="font-size: 17px"> Profil</span>
-                </a>
-            </div>
-            <div>
-                <a href="{{ route('perizinan') }}" class="text-decoration-none text-white">
-                    <img src="{{ asset('assets/icon/perizinan.png') }}" alt="" class="nav-custom">
-                    <span style="font-size: 15px">Perizinan</span>
-                </a>
-
-            </div>
-            <div>
-                <a href="{{ route('ganti-password') }}" class="text-decoration-none text-white">
-                    <img src="{{ asset('assets/icon/change-password.svg') }}" alt="" class=" nav-custom">
-                    <span style="font-size: 15px"> Ganti Password</span>
-                </a>
+        <div class="sidebar text-white">
+            <div class="header-box custom-profile mb-3 pb-3">
+                <div class="mb-2 p-3" style="z-index: 2">
+                    <img src="{{ asset('assets/img/profile-pict.png') }}" alt="profile" class="rounded-circle"
+                        width="80px" height="80px">
+                </div>
+                <div class="p-2" style="z-index: 2">
+                    <span class="fw-bold nama">Weldy Flaminggo</span><br>
+                    <span class="nim">123456789</span>
+                </div>
             </div>
 
-            <hr>
-            <a href="{{route('splashscreen')}}" class="text-decoration-none text-white" id="logout">
-                <img src="{{ asset('assets/icon/logout.svg') }}" alt="" class=" nav-custom">
-                <span style="font-size: 15px"> Logout</span>
-            </a>
+            <!-- NAVIGASI -->
+            <div class="d-flex flex-column m-0 ">
+                <div>
+                    <a href="{{ route('dashboard') }}" class="text-decoration-none text-white">
+                        <img src="{{ asset('assets/icon/absensi.png') }}" alt="" class="nav-custom">
+                        <span style="font-size: 15px"> Absen</span>
+                    </a>
+                </div>
+                <div>
+                    <a href="{{ route('profil') }}" class="text-decoration-none text-white ">
+                        <img src="{{ asset('assets/icon/profil.png') }}" alt="" class="nav-custom">
+                        <span style="font-size: 17px"> Profil</span>
+                    </a>
+                </div>
+                <div>
+                    <a href="{{ route('jenis-absen') }}" class="text-decoration-none text-white">
+                        <img src="{{ asset('assets/icon/perizinan.png') }}" alt="" class="nav-custom">
+                        <span style="font-size: 15px">Perizinan</span>
+                    </a>
+                </div>
+                <hr class="m-0">
+                <a href="{{ route('splashscreen') }}" class="text-decoration-none text-white" id="logout">
+                    <img src="{{ asset('assets/icon/logout.svg') }}" alt="" class=" nav-custom">
+                    <span style="font-size: 15px"> Logout</span>
+                </a>
+            </div>
+            <!-- END -->
         </div>
-    </div>
-    <script>
-        function toggleSidebar() {
-            var sidebar = document.querySelector('.sidebar');
-            var appbar = document.querySelector('.appbar');
-            var menuToggle = document.querySelector('#menuToggle');
-            var content = document.getElementById('content'); // Added content reference
+    </header>
+    <script src="{{asset('assets/js/logout.js')}}"></script>
 
-            if (menuToggle.checked) {
-                sidebar.style.width = '350px';
-                appbar.style.marginLeft = '350px';
-                content.style.marginLeft = '350px'; // Adjust content margin
-            } else {
-                sidebar.style.width = '0';
-                appbar.style.marginLeft = '0';
-                content.style.marginLeft = '0'; // Reset content margin
-            }
-        }
-
-        // Event handler for the logout button
-        document.getElementById('logout').addEventListener('click', function() {
-            Swal.fire({
-                title: 'Do you want to log out?',
-                showDenyButton: true,
-                showCancelButton: true,
-                confirmButtonText: 'Log Out',
-                denyButtonText: `Cancel`,
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    // Perform the logout action here
-                    Swal.fire('Logged Out!', '', 'success');
-                }
-            });
-        });
-    </script>
