114eeb51973a81b0de0530a2d6b6707b1bc370ba
Rudiyat <119860950+pddccvv@users.noreply.github.com>
2023-11-07 09:55:29 +0700

    pemindai update

--- front_end/public/assets/js/mahasiswa/pemindai.js
+++ front_end/public/assets/js/mahasiswa/pemindai.js
@@ -1,66 +1,61 @@
+// Pada file JavaScript Anda (pemindai.js)
 let selectedDeviceId = null;
-    const codeReader = new ZXing.BrowserMultiFormatReader();
-    const sourceSelect = $("#pilihKamera");
+const codeReader = new ZXing.BrowserMultiFormatReader();
+const sourceSelect = $("#pilihKamera");
 
-    $(document).on('change', '#pilihKamera', function () {
-        selectedDeviceId = $(this).val();
-        if (codeReader) {
-            codeReader.reset();
-            initScanner();
-        }
-    });
-
-    function initScanner() {
-        codeReader
-            .listVideoInputDevices()
-            .then(videoInputDevices => {
-                videoInputDevices.forEach(device =>
-                    console.log(`${device.label}, ${device.deviceId}`)
-                );
-
-                if (videoInputDevices.length > 0) {
-                    if (selectedDeviceId == null) {
-                        if (videoInputDevices.length > 1) {
-                            selectedDeviceId = videoInputDevices[1].deviceId;
-                        } else {
-                            selectedDeviceId = videoInputDevices[0].deviceId;
-                        }
-                    }
-
-                    if (videoInputDevices.length >= 1) {
-                        sourceSelect.html('');
-                        videoInputDevices.forEach((element) => {
-                            const sourceOption = document.createElement('option');
-                            sourceOption.text = element.label;
-                            sourceOption.value = element.deviceId;
-                            if (element.deviceId == selectedDeviceId) {
-                                sourceOption.selected = true; // Use 'true' instead of 'selected'
-                            }
-                            sourceSelect.append(sourceOption);
-                        });
-                    }
-
-                    codeReader
-                        .decodeOnceFromVideoDevice(selectedDeviceId, 'previewKamera')
-                        .then(result => {
-                            // Handle the scanned result
-                            console.log(result.text);
-                            $("#hasilscan").val(result.text);
-
-                            if (codeReader) {
-                                codeReader.reset();
-                            }
-                        })
-                        .catch(err => console.error(err));
-                } else {
-                    alert("Camera not found!");
-                }
-            })
-            .catch(err => console.error(err));
-    }
-
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+$(document).on("change", "#pilihKamera", function () {
+    selectedDeviceId = $(this).val();
+    if (codeReader) {
+        codeReader.reset();
         initScanner();
-    } else {
-        alert('Cannot access camera.');
     }
+});
+
+function initScanner() {
+    codeReader
+        .listVideoInputDevices()
+        .then((videoInputDevices) => {
+            if (videoInputDevices.length > 0) {
+                if (selectedDeviceId === null) {
+                    selectedDeviceId = videoInputDevices[0].deviceId;
+                }
+
+                sourceSelect.html(""); // Clear the options before adding new ones.
+                videoInputDevices.forEach((element) => {
+                    const sourceOption = document.createElement("option");
+                    sourceOption.text = element.label;
+                    sourceOption.value = element.deviceId;
+                    if (element.deviceId === selectedDeviceId) {
+                        sourceOption.selected = true;
+                    }
+                    sourceSelect.append(sourceOption);
+                });
+
+                codeReader
+                    .decodeFromVideoDevice(selectedDeviceId, "previewKamera", (result) => {
+                        // Handle the scanned result
+                        console.log(result.text);
+                        $("#hasilscan").val(result.text);
+
+                        // Continue scanning after a short delay
+                        setTimeout(initScanner, 1000);
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                        alert("Error accessing camera or scanning.");
+                    });
+            } else {
+                alert("No video input devices found.");
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            alert("Error listing video input devices.");
+        });
+}
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    initScanner();
+} else {
+    alert("Cannot access camera. Make sure your browser supports getUserMedia and has necessary permissions.");
+}
--- front_end/resources/views/mahasiswa/pemindai.blade.php
+++ front_end/resources/views/mahasiswa/pemindai.blade.php
@@ -12,14 +12,14 @@
     <script type="text/javascript" src="https://unpkg.com/@zxing/library@latest"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{ asset('assets/js/mahasiswa/pemindai.js') }}"></script>
     </section>
 
-    <div>
-        <video id="previewKamera" style="width: 300px; height: 300px;"></video>
+    <div class="border m-2 bg-danger" style="margin-top: 150px;">
+        <video id="previewKamera" style="width: 100%; height: 85%;"></video>
         <br>
         <select id="pilihKamera" style="max-width: 400px"></select>
         <br>
         <input type="text" id="hasilscan">
-        <a href="{{ route('dashboard') }}" class="btn btn-primary">Batal</a>
     </div>
